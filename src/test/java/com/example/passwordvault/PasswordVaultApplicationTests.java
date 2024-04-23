6ba78f1fe61a017885e2bac0da4f297b831c1931
Erin.Schneider <erinsuzy@gmail.com>
2024-04-23 18:40:32 -0500

    started passwordvault

---
+++ src/test/java/com/example/passwordvault/PasswordVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.passwordvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
